eb0ecf64ce5c79e60d06949ec8750970007be19a
Fumiya Sakai <just1factory@gmail.com>
2019-02-10 17:46:57 +0900

    第3章：PurchasePresentContentsプロジェクト追加

---
+++ 03_PurchasePresentContents/PurchasePresentContents/PurchasePresentContents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PurchasePresentContents
+//
+//  Created by 酒井文也 on 2019/02/10.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
