026218dd3e6d8d93b4a8c28d459c01fa8f8e1113
Fumiya Sakai <just1factory@gmail.com>
2019-02-23 14:17:29 +0900

    第2章: 必要なファイルの追加とライブラリの変更

---
+++ 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContents/Enum/ArticleCategories.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleCategories.swift
+//  MediaStyleAnimationContents
+//
+//  Created by 酒井文也 on 2019/02/23.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import Foundation
+
+enum ArticleCategories: CaseIterable {
+    
+}
