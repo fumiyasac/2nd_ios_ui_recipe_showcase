0bfb6ce2333718c8bcd3baa747bd47855c32cf92
Fumiya Sakai <just1factory@gmail.com>
2019-02-23 14:39:18 +0900

    第2章: Main.storyboard関連部分組み立て

---
+++ 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContents/ViewController/MainArticleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainArticleViewController.swift
+//  MediaStyleAnimationContents
+//
+//  Created by 酒井文也 on 2019/02/23.
+//  Copyright © 2019 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+final class MainArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
