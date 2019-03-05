9e7c7454cc2ff25c1bc73081db44ee96e9c00430
Fumiya Sakai <just1factory@gmail.com>
2019-03-05 23:25:23 +0900

    第2章: モック用サーバーの準備

--- 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContents/ViewController/Main/ContainerView/MainSliderViewController.swift
+++ 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContents/ViewController/Main/ContainerView/MainSliderViewController.swift
@@ -7,12 +7,68 @@
 //
 
 import UIKit
+import FSPagerView
+import AlamofireImage
 
 final class MainSliderViewController: UIViewController {
+
+    private var mainSliderLists: [MainSliderEntity] = [] {
+        didSet {
+            self.mainSliderView.reloadData()
+        }
+    }
+
+    @IBOutlet weak private var mainSliderView: FSPagerView!
 
     // MARK: - Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupSliderView()
+    }
+
+    // MARK: - Private Function
+
+    private func setupSliderView() {
+        mainSliderView.delegate = self
+        mainSliderView.dataSource = self
+        mainSliderView.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 180)
+        mainSliderView.interitemSpacing = 16
+        mainSliderView.transformer = FSPagerViewTransformer(type: .depth)
+    }
+}
+
+// MARK: - FSPagerViewDataSource, FSPagerViewDelegate
+
+extension MainSliderViewController: FSPagerViewDataSource, FSPagerViewDelegate {
+
+    // MEMO: UICollectionViewの配置対象のセクションの個数を設定する部分に相当
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        return mainSliderLists.count
+    }
+
+    // MEMO: UICollectionViewの配置対象のセクションに配置するセルの個数を設定する部分に相当する
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+
+        let mainSlider = mainSliderLists[index]
+        if let imageUrl = mainSlider.imageUrl {
+            cell.imageView?.af_setImage(withURL: imageUrl)
+            cell.imageView?.contentMode = .scaleAspectFill
+            cell.imageView?.clipsToBounds = true
+        }
+        cell.textLabel?.text = mainSlider.title
+        cell.contentView.layer.shadowOpacity = 0.4
+        cell.contentView.layer.opacity = 0.86
+
+        return cell
+    }
+
+    // MEMO: UICollectionViewの配置対象のセクション押下時の処理を設定する部分に相当する
+    func pagerView(_ pagerView: FSPagerView, didSelectItemAt index: Int) {
+        pagerView.deselectItem(at: index, animated: true)
+        pagerView.scrollToItem(at: index, animated: true)
     }
 }
--- 02_MediaStyleAnimationContents/server_mock/index.js
+++ 02_MediaStyleAnimationContents/server_mock/index.js
@@ -6,7 +6,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 // Database構築用のJSONファイル
-const router = jsonServer.router('datasource/db.json');
+const router = jsonServer.router('datasources/db.json');
 
 // 各種設定用
 const middlewares = jsonServer.defaults();
@@ -16,6 +16,22 @@ const rewrite_rules = jsonServer.rewriter({
     "/api/v1.0/articles/sliders"         : "/get_sliders_list",
     "/api/v1.0/articles/recommend"       : "/get_recommend_list",
 });
+
+// リクエストのルールを設定する
+server.use(rewrite_rules);
+
+// ミドルウェアを設定する (※コンソール出力するロガーやキャッシュの設定等)
+server.use(middlewares);
+
+// 受信したリクエストにおいてGET送信時のみ許可する
+server.use(function (req, res, next) {
+    if (req.method === 'GET') {
+        next();
+    }
+});
+
+// ルーティングを設定する
+server.use(router);
 
 // サーバをポート3000で起動する
 server.listen(3000, () => {
