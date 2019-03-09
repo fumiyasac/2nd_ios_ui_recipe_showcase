41e7e5fbad9c12394fa0ec79e15831d5d2432cbd
Fumiya Sakai <just1factory@gmail.com>
2019-03-10 02:35:59 +0900

    第2章: テストコードに関する追記

--- 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContentsTests/MediaStyleAnimationContentsTests.swift
+++ 02_MediaStyleAnimationContents/MediaStyleAnimationContents/MediaStyleAnimationContentsTests/MediaStyleAnimationContentsTests.swift
@@ -27,7 +27,7 @@ class MediaStyleAnimationContentsTests: XCTestCase {
         XCTAssertEqual(0, viewModel.targetMainSliderLists.count, "フェッチ実行前は変数:targetMainSliderListsの総数は0となること")
 
         // スライダー表示用画像データのフェッチを実行する
-        fetchMainSlider(viewModel: viewModel, timeOutSec: 10.0)
+        fetchMainSliderLists(viewModel: viewModel, timeOutSec: 15.0)
     }
 
     // Case2: 記事データの取得処理に関するテスト
@@ -42,14 +42,32 @@ class MediaStyleAnimationContentsTests: XCTestCase {
 
         // 記事データのフェッチを実行する
         for page in 1...3 {
-            fetchMainArticle(page: page, viewModel: viewModel, timeOutSec: 15.0)
+            fetchMainArticleLists(page: page, viewModel: viewModel, timeOutSec: 15.0)
         }
+    }
+
+    // Case3: 詳細ページのデータの取得処理に関するテスト
+    func testDetailViewModel() {
+        
+        // 必要なViewModelを初期化する(APIアクセス部分はMockを利用)
+        let viewModel = DetailViewModel(notificationCenter: NotificationCenter(), api: MockArticleAPIManager.shared)
+
+        // フェッチを実行前の初期状態
+        XCTAssertNil(viewModel.targetDetailArticle, "フェッチ実行前は変数:targetDetailArticleはnilとなること")
+        XCTAssertEqual(0, viewModel.targetDetailRecommendLists.count, "フェッチ実行前は変数:targetDetailRecommendListsの総数は0となること")
+
+        // 対象のIDに紐づく記事データのフェッチを実行する
+        let targetId = Int.random(in: 1 ... 30)
+        fetchDetailArticleBy(id: targetId, viewModel: viewModel, timeOutSec: 15.0)
+
+        // おすすめ記事データのフェッチを実行する
+        fetchRecommend(viewModel: viewModel, timeOutSec: 15.0)
     }
 
     // MARK: - Private Function
 
     // Case1: スライダー表示用画像データの取得処理を実行
-    private func fetchMainSlider(viewModel: MainSliderViewModel, timeOutSec: TimeInterval) {
+    private func fetchMainSliderLists(viewModel: MainSliderViewModel, timeOutSec: TimeInterval) {
 
         let expectation: XCTestExpectation? = self.expectation(description: "fetchMainSliderExpectation")
 
@@ -70,7 +88,7 @@ class MediaStyleAnimationContentsTests: XCTestCase {
     }
 
     // Case2: 記事データの取得処理を実行
-    private func fetchMainArticle(page: Int, viewModel: MainArticleViewModel, timeOutSec: TimeInterval) {
+    private func fetchMainArticleLists(page: Int, viewModel: MainArticleViewModel, timeOutSec: TimeInterval) {
 
         let descriptionName = "fetchMainSliderExpectation\(page)"
         let expectation: XCTestExpectation? = self.expectation(description: descriptionName)
@@ -96,4 +114,51 @@ class MediaStyleAnimationContentsTests: XCTestCase {
         })
     }
 
+    // Case3: 詳細ページのデータの取得処理を実行する(その1)
+    private func fetchDetailArticleBy(id: Int, viewModel: DetailViewModel, timeOutSec: TimeInterval) {
+        
+        let descriptionName = "fetchArticleById\(id)"
+        let expectation: XCTestExpectation? = self.expectation(description: descriptionName)
+
+        // サブスレッドでViewModelのメソッドを実行する
+        DispatchQueue.global().async {
+            viewModel.fetchDetailArtcleBy(targetId: id)
+            DispatchQueue.main.async {
+                expectation?.fulfill()
+            }
+        }
+
+        // 引数で指定したタイムアウト時間内に処理された場合の検証を実行する
+        waitForExpectations(timeout: timeOutSec, handler: { _ in
+
+            // テスト結果として期待する値や結果に関する定義
+            let expectedResult = (id == viewModel.targetDetailArticle?.id)
+
+            // MEMO: ViewModelの変数:targetDetailArticleで引数で渡したIDと取得データのIDが等しいかを確認する
+            XCTAssertTrue(expectedResult, "引数で渡したIDと取得データのIDが等しくなること")
+        })
+    }
+
+    // Case3: 詳細ページのデータの取得処理を実行する(その2)
+
+    private func fetchRecommend(viewModel: DetailViewModel, timeOutSec: TimeInterval) {
+
+        let descriptionName = "fetchRecommend"
+        let expectation: XCTestExpectation? = self.expectation(description: descriptionName)
+
+        // サブスレッドでViewModelのメソッドを実行する
+        DispatchQueue.global().async {
+            viewModel.fetchRecommend()
+            DispatchQueue.main.async {
+                expectation?.fulfill()
+            }
+        }
+
+        // 引数で指定したタイムアウト時間内に処理された場合の検証を実行する
+        waitForExpectations(timeout: timeOutSec, handler: { _ in
+
+            // MEMO: ViewModelの変数:targetDetailRecommendListsにスタブのJSONファイルから取得したデータが反映されているかを確認する
+            XCTAssertEqual(6, viewModel.targetDetailRecommendLists.count, "フェッチ実行前は変数:targetDetailRecommendListsの総数は6となること")
+        })
+    }
 }
