173e4dfa10b92524c8909c6ef9f9ad6602b5c7e9
Fumiya Sakai <just1factory@gmail.com>
2019-11-30 22:45:10 +0900

    (Xcode11対応)第3章: AppDelegateへの追記対応

--- 03_PurchasePresentContents/PurchasePresentContents/PurchasePresentContents/AppDelegate.swift
+++ 03_PurchasePresentContents/PurchasePresentContents/PurchasePresentContents/AppDelegate.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
+// 本サンプルにおいて現状非対応にしている、または最低限の対応だけに留めている部分は下記の通り
+// 1. DarkMode: 現状ではDarkModeをキャンセルしています。
+// 2. UISceneAPI: 現状挙動への問題はないがiOS13以降では非推奨となっています。
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // MEMO: DarkModeのキャンセル（Info.plistについても念のため記載）
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         return true
     }
 
